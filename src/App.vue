337aa71b425b77adc2edfdf4d1248bea2523b67e
Panda L <jbvhl@hotmail.com>
2019-03-26 16:04:32 -0600

    donzo

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <button @click="toggle(true)">Create</button>
+      <h1>Battle Bots</h1>
+      <button @click='toggle(false)'>Collection</button>
+      <section v-if="showForm">
+        <input placeholder="Bot Name*" v-model="bot">
+        <input placeholder="Attack Value*" v-model="attack">
+        <input placeholder="HealthValue*" v-model="health">
+        <button @click="addBot">Submit</button>
+        <button @click="clearBot">Clear</button>
+      </section>
+      <section v-else>
+        <h2 v-if="selectedBots[0]">Bot #1: {{selectedBots[0].bot}}</h2>
+        <h2 v-else>Bot #1: Select a bot below</h2>
+        <h2 v-if="selectedBots[1]">Bot #2: {{selectedBots[1].bot}}</h2>
+        <h2 v-else>Bot #2: Select a bot below</h2>
+        <button @click='battle'>Battle</button>
+        <button @click='clearSelected'>Clear</button>
+        <bot v-for='(botObj, i) in allBots' :key='i' :index='i' :botObj='botObj' :deleteBot='deleteBot' :selectBot='selectBot'/>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Bot from './components/Bot';
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      showForm: true,
+      bot: "",
+      attack: 0,
+      health: 0,
+      allBots: [],
+      selectedBots: []
+    };
+  },
+
+  methods: {
+    toggle(val) {
+      this.showForm = val;
+    },
+
+    addBot() {
+      const newBot = {
+        bot: this.bot,
+        attack: this.attack,
+        health: this.health
+      };
+      this.allBots.push(newBot);
+      this.bot = "";
+      this.attack = 0;
+      this.health = 0;
+      this.toggle();
+    },
+    clearBot() {
+      this.bot = "";
+      this.attack = 0;
+      this.health = 0;
+    },
+
+    clearSelected() {
+      this.selectedBots = [];
+    },
+
+    deleteBot(i) {
+      this.allBots.splice(i, 1)
+    },
+
+    selectBot(botObj) {
+      if (this.selectedBots.length < 2) {
+        this.selectedBots.push(botObj)
+      } else {
+        alert('2 bots already selected');
+      }
+    },
+
+    battle() {
+      if (this.selectedBots[0].attack > this.selectedBots[1].attack) {
+        alert(`${this.selectedBots[0].bot} wins!`);
+      } else {
+        alert(`${this.selectedBots[1].bot} wins!`);
+      }
+      this.selectedBots = [];
+    }
+  },
   components: {
-    HelloWorld
+    Bot
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Bot.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3>{{botObj.bot}}</h3>
+        <p>{{botObj.attack}}</p>
+        <p>{{botObj.health}}</p>
+        <button @click='selectBot(botObj)'>Select</button>
+        <button @click="deleteBot(index)">Retire</button>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+props: ['botObj', 'deleteBot', 'selectBot', 'index']
+}
+</script>
